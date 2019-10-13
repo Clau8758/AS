458c5892de48f9c5c4ce60df4c300eb7f117632f
Clau8758 <56257587+Clau8758@users.noreply.github.com>
2019-10-13 22:46:29 +0200

    Update Mini_project.py

--- Mini_project.py
+++ Mini_project.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python
 import roslib
 roslib.load_manifest('hello_ros')
-import motionplanner.py
-import Block_classes.py
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+import shape_msgs.msg as shape_msgs
+from sensor_msgs.msg import JointState
+from numpy import zeros, array, linspace
+from math import ceil
+from std_msgs.msg import String
+from gazebo_msgs.srv import GetModelState
+from Motionplanner import *
+from Block_classes import *
+#from Block_classes import container 
 
 
-currentJointState = JointState()
-
+tuto = container()
 
 if __name__ == '__main__':
-    tuto = container()
     model_coordinates = rospy.ServiceProxy('/gazebo/get_model_state', GetModelState)
     cube_coord=[]
     for block in tuto._blockListDict.itervalues():
@@ -18,22 +29,10 @@ if __name__ == '__main__':
       resp_coordinates = (model_coordinates(blockName, block._relative_entity_name))
       if (resp_coordinates.success):
           cube_coord.append(model_coordinates(blockName, block._relative_entity_name))
-          print '\n'
-          print 'Status.success = ', resp_coordinates.success
-          print(blockName)
-          print("Cube " + str(block._name))
-          print("Valeur de X : " + str(resp_coordinates.pose.position.x))
-          print("Quaternion y : " + str(resp_coordinates.pose.position.y))
-          print("Quaternion z : " + str(resp_coordinates.pose.position.z))
-          print("Valeur de X : " + str(resp_coordinates.pose.orientation.x))
-          print("Quaternion y : " + str(resp_coordinates.pose.orientation.y))
-          print("Quaternion z : " + str(resp_coordinates.pose.orientation.z))
-          print("Quaternion w : " + str(resp_coordinates.pose.orientation.w))
-          print("Cube " + str(block._name))
           i=i+1;
-    try:
+try:
       move_group_python_mini_project()
-    except rospy.ROSInterruptException:
+except rospy.ROSInterruptException:
       pass
 
 
